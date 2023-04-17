8d5177e85159378f7c7287b22c58bb192c9106e5
AshokAndani <andani.ashok007@gmail.com>
2023-04-17 09:39:38 +0530

    [ashok]fix: added factrial program

---
+++ Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionPrograms
+{
+    internal class Factorial
+    {
+        public int Fact(int n)
+        {
+            int value = 1;
+            while (n > 0)
+            {
+                value *= n--;
+            }
+            return value;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter 1 for Factorial:");
+            int input = int.Parse(Console.ReadLine());
+            switch (input)
+            {
+                case 1:
+                    Console.Write("Enter the input for factorial: ");
+                    int inp = int.Parse(Console.ReadLine());
+                    Factorial factorial = new Factorial();
+                    int result = factorial.Fact(inp);
+                    Console.WriteLine($"factorial of {inp} is : {result}");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry");
+                    break;
+            }
         }
     }
 }
